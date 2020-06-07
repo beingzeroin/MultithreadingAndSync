09d790a2a0751f9a8cd8157c9282e4cf4ae1e8fb
Sandeep Wadhwa <sandeep.sajiru@gmail.com>
2020-06-07 10:24:42 +0530

    Multi-Threaded Program Added

--- BeingZeroLogSearch.java
+++ BeingZeroLogSearch.java
@@ -3,45 +3,103 @@ import java.io.*;
 
 class Configuration{
     public static final String LOG_FOLDER_PATH = "./spring-boot-server-logs";
+    public static final LogLevel LOG_LEVEL = LogLevel.DEBUG;
 }
 
 class FileUtility {
-    public static List<String> getAllLogFiles(String folderPath){
+    public static List<File> getAllLogFiles(String folderPath){
         File folder = new File(folderPath);
         File[] files = folder.listFiles();
-        List<String> filePaths = new ArrayList<>();
-        for(File f : files)
-            filePaths.add(f.getAbsolutePath());
-        return filePaths;
+        return Arrays.asList(files);
     }
 }
 
-interface ILogSearch {
-    public int logSearch(String pattern) throws Exception;
+enum LogLevel{
+    DEBUG, INFO, WARN, FATAL
 }
-class SingleThreadedLogSearch implements ILogSearch{
-    public SingleThreadedLogSearch(){
+class Logger{
+    LogLevel logLevel;
+    Logger(){
+        logLevel = Configuration.LOG_LEVEL;
     }
-    public int logSearch(String pattern) throws Exception {
-        int count = 0;
-        List<String> filePaths = FileUtility.getAllLogFiles(Configuration.LOG_FOLDER_PATH);
-        for(String path : filePaths){
-            System.out.println("Searching "+path);
-            BufferedReader br = new BufferedReader(new FileReader(path));
-            int currentCount = 0;
+    public void info(String msg){
+        System.out.println(msg);
+    }
+    public void warn(String msg){
+        System.out.println(msg);
+    }
+    public void debug(String msg){
+        if(logLevel == LogLevel.DEBUG)
+            System.out.println(msg);
+    }
+    public void fatal(String msg){
+        System.out.println(msg);
+    }
+}
+
+class FileWordCount {
+    static Logger logger = new Logger();
+    public static int wordCount(File file, String pattern){
+        int currentCount = 0;
+        String filePath = file.getAbsolutePath();
+        try{
+            BufferedReader br = new BufferedReader(new FileReader(filePath));
             String line;
             while((line = br.readLine()) != null){
                 if(line.contains(pattern))
                     currentCount++;
             }
-            count += currentCount;
         }
+        catch(Exception ex){
+            ex.printStackTrace();
+        }
+        logger.debug(String.format("'%s' : '%s' => %d\n", filePath, pattern, currentCount));
+        return currentCount;
+    }
+}
+interface ILogSearch {
+    public int logSearch(String pattern) throws Exception;
+}
+
+class SingleThreadedLogSearch implements ILogSearch{
+    public SingleThreadedLogSearch(){
+    }
+    public int logSearch(String pattern) throws Exception {
+        int count = 0;
+        List<File> files = FileUtility.getAllLogFiles(Configuration.LOG_FOLDER_PATH);
+        for(File file : files){
+            count += FileWordCount.wordCount(file, pattern);
+        }
+        return count;
+    }
+}
+
+class MultithreadedLogSearch implements ILogSearch{
+    int count;
+    public MultithreadedLogSearch(){
+        count = 0;
+    }
+    public int logSearch(String pattern) throws Exception {
+        List<File> files = FileUtility.getAllLogFiles(Configuration.LOG_FOLDER_PATH);
+        List<Thread> threadsList = new ArrayList<>();
+        for(File file : files){
+            threadsList.add(new Thread(){
+                public void run(){
+                    count += FileWordCount.wordCount(file, pattern);
+                }
+            });
+        }
+        for(Thread t : threadsList)
+            t.start();
+        for(Thread t : threadsList)
+            t.join();
         return count;
     }
 }
 
 class TimeDecorator implements ILogSearch{ 
     ILogSearch decoratee;
+    static Logger logger = new Logger();
     public TimeDecorator(ILogSearch decoratee){
         this.decoratee = decoratee;
     }
@@ -49,17 +107,21 @@ class TimeDecorator implements ILogSearch{
         long startTime = System.currentTimeMillis();
         int ans = decoratee.logSearch(pattern);
         long endTime = System.currentTimeMillis();
-        System.out.println("Time Taken: "+ (endTime-startTime) + " ms");
+        logger.info("Time Taken: "+ (endTime-startTime) + " ms");
         return ans;
     } 
 }
 
 public class BeingZeroLogSearch{
     public static void main(String args[]) throws Exception {
-        ILogSearch stls = new SingleThreadedLogSearch();
-        ILogSearch td = new TimeDecorator(stls);
+        Logger logger = new Logger();
+        ILogSearch td = new TimeDecorator(new SingleThreadedLogSearch());
         String searchTerm = "MongoSocketOpenException";
         searchTerm = "mongodb";
-        System.out.println("\'"+searchTerm+"\'" + " occurs "+ td.logSearch(searchTerm) + " times.");
+        logger.info("\'"+searchTerm+"\'" + " occurs "+ td.logSearch(searchTerm) + " times.");
+    
+        td = new TimeDecorator(new MultithreadedLogSearch());
+        logger.info("\'"+searchTerm+"\'" + " occurs "+ td.logSearch(searchTerm) + " times.");
+        
     }
 }
